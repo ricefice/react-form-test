91d21302505fc50d6b8f39fdb305a5a5a467515d
Tom Oh <96092736+TDHOh@users.noreply.github.com>
2022-05-21 14:03:58 +1200

    setup basic test with readme

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Form Test
       </header>
+      <div>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+        }}>
+          <div>
+            {
+              ['1', '10', '100'].map(val => (
+                <button
+                  key={val}
+                  onClick={(e) => {
+                    console.log(val)}
+                  }
+                >{val}</button>
+              ))
+            }
+          </div>
+          <div>
+            <input
+              type='string'
+              // onKeyDown={e => e.preventDefault}
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
